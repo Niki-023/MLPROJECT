770a89ba49786d05329a322cdebf04ddc048ca63
Niki-023 <nikk.23panchal@gmail.com>
2026-05-23 16:22:39 -0400

    Model Trainer

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sys
 from src.mlproject.components.data_ingestion import DataIngestion
 #from src.mlproject.components.data_ingestion import DataIngestionConfig
 from src.mlproject.components.data_transformation import DataTransformationConfig, DataTranformation
+from src.mlproject.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 
 
@@ -19,7 +20,11 @@ if __name__ =="__main__":
 
         #data_transformation_config = DataTransformationConfig()
         data_transformation = DataTranformation()
-        data_transformation.initate_data_transformation(train_data_path,test_data_path)
+        train_arr, test_arr,_ =data_transformation.initate_data_transformation(train_data_path,test_data_path)
+
+        ## Model Training
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
 
     except Exception as e:
